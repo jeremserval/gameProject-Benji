f1bd9fae0993a4652bda20b0aa3cc14a8db44804
BenjaminServal <benjiserval@gmail.com>
2021-04-15 01:17:48 +0200

    add api, Search Bar and home page with GameList

---
+++ game/src/components/SearchBar/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const SearchBar = ({completion}) => {
+  return (
+    <div className="SearchBar">
+      <form action="/" method="get">
+        <label htmlFor="header-search">
+            <span className="visually-hidden">Search game </span>
+        </label>
+        <input
+            onInput={e => completion(e.target.value)}    
+            type="text"
+            id="header-search"
+            placeholder="Search"
+            name="" 
+        />
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
